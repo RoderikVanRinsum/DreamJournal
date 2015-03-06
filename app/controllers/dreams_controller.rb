c26d2559e1ac32d34808d3999d9b802acc155b0d
Roderik van Rinsum <rhvanrinsum@gmail.com>
2015-03-06 14:44:30 +0100

    Start form

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -6,4 +6,20 @@ class DreamsController < ApplicationController
   def show
     @dream = Dream.find(params[:id])
   end
+
+  def create
+    @dream = Dream.new(dream_params)
+
+    if @dream.save
+      flash[:success] = 'Dream was successfully added'
+      redirect_to @dream
+    else
+      render 'index'
+    end
+  end
+
+  private
+    def dream_params
+      params.require(:dream).permit(:name, :description, :date)
+    end
 end
