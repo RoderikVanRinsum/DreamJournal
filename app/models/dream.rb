ed855d520205a62cbcfa0324471d5e0879313b38
Roderik van Rinsum <rhvanrinsum@gmail.com>
2015-03-16 12:17:37 +0100

    Create relationships between dreams, dreamsigns, dream_dreamsigns

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -1,4 +1,7 @@
 class Dream < ActiveRecord::Base
+  has_many :dream_dreamsigns, dependent: :destroy
+  has_many :dreamsigns, through: :dream_dreamsigns
+
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
 end
--- app/models/dreamsign.rb
+++ app/models/dreamsign.rb
@@ -1,2 +1,4 @@
 class Dreamsign < ActiveRecord::Base
+  has_many :dream_dreamsigns, dependent: :destroy
+  has_many :dreams, through: :dream_dreamsigns
 end
