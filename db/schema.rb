3a9830f1dccc9fad39e2c74b738100378e4abc2a
Roderik van Rinsum <rhvanrinsum@gmail.com>
2015-03-24 16:09:04 +0100

    Run user model migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150318125651) do
+ActiveRecord::Schema.define(version: 20150324150717) do
 
   create_table "dream_dreamsigns", force: :cascade do |t|
     t.integer  "dream_id"
@@ -38,6 +38,13 @@ ActiveRecord::Schema.define(version: 20150318125651) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
