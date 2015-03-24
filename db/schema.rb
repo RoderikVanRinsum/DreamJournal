5e690701e46cad71048bcb0321aaae115fc8afe5
Roderik van Rinsum <rhvanrinsum@gmail.com>
2015-03-24 17:00:45 +0100

    Add password digest to users, create + run migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150324154124) do
+ActiveRecord::Schema.define(version: 20150324160008) do
 
   create_table "dream_dreamsigns", force: :cascade do |t|
     t.integer  "dream_id"
@@ -43,8 +43,9 @@ ActiveRecord::Schema.define(version: 20150324154124) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
