72ac658b3cb8ef8699e8f15093a2b5839d625391
Roderik van Rinsum <rhvanrinsum@gmail.com>
2015-03-24 11:56:11 +0100

    Add more layout tests

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,10 +1,19 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+
   test 'layout links' do
     get root_path
     assert_template 'dreams/index'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", dreamsigns_path
   end
+
+  test 'layout elements' do
+    get root_path
+    assert_select "a#logo"
+    assert_select "section.main-content"
+    assert_select "aside.search-sidebar"
+  end
+
 end
