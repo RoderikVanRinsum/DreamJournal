cd5eb85ca2e36ea311477bef793e88ba15f4ff2d
Roderik van Rinsum <rhvanrinsum@gmail.com>
2015-03-25 19:43:30 +0100

    Create and run add remember digest to users migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150324160008) do
+ActiveRecord::Schema.define(version: 20150325184227) do
 
   create_table "dream_dreamsigns", force: :cascade do |t|
     t.integer  "dream_id"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20150324160008) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
