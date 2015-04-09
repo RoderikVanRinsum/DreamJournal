241aecd23636446da784a793ed9057723311965e
Roderik van Rinsum <rhvanrinsum@gmail.com>
2015-04-09 18:59:58 +0200

    Add Dream factory and pass 'has a valid factory' spec

--- spec/models/dream_spec.rb
+++ spec/models/dream_spec.rb
@@ -2,7 +2,9 @@
 require 'rails_helper'
 
 describe Dream do
-  it "has a valid factory"
+  it "has a valid factory" do
+    expect(FactoryGirl.create(:dream)).to be_valid
+  end
   it "is invalid without name"
   it "is invalid without description"
   it "returns a dream's keywords as a string"
