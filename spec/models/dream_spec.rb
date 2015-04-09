13552ea69e3dca4dc667a67172b2b8a602d80285
Roderik van Rinsum <rhvanrinsum@gmail.com>
2015-04-09 18:49:02 +0200

    Add dream_spec with 4 pending specs

---
+++ spec/models/dream_spec.rb
@@ -0,0 +1,9 @@
+# spec/models/dream.rb
+require 'rails_helper'
+
+describe Dream do
+  it "has a valid factory"
+  it "is invalid without name"
+  it "is invalid without description"
+  it "returns a dream's keywords as a string"
+end
