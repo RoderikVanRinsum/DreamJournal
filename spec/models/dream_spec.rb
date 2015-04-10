99e890cc0ff339f006c49e1a09dd59361dac8d0b
Roderik van Rinsum <rhvanrinsum@gmail.com>
2015-04-10 13:19:16 +0200

    Add dream keywords test

--- spec/models/dream_spec.rb
+++ spec/models/dream_spec.rb
@@ -2,12 +2,16 @@
 require 'rails_helper'
 
 describe Dream do
+
   it "has a valid factory" do
     expect(FactoryGirl.create(:dream)).to be_valid
   end
+
+  #validation tests
   it "is invalid without name" do
     expect(FactoryGirl.build(:dream, name: nil)).not_to be_valid
   end
+
   it "is invalid without description" do
     expect(FactoryGirl.build(:dream, description: nil)).not_to be_valid
   end
@@ -16,5 +20,9 @@ describe Dream do
     FactoryGirl.create(:dream, name: "Double")
     expect(FactoryGirl.build(:dream, name: "Double")).not_to be_valid
   end
-  it "returns a dream's keywords as a string"
+
+  it "returns a dream's keywords as a string" do
+    dream = FactoryGirl.create(:dream, name: "Name", description: "desc")
+    expect(dream.keywords).to eq("name desc")
+  end
 end
