1e0e77eab1de0a285ec8080ccdf224cba706b402
Roderik van Rinsum <rhvanrinsum@gmail.com>
2015-04-10 13:09:20 +0200

    add validation specs

--- spec/models/dream_spec.rb
+++ spec/models/dream_spec.rb
@@ -5,7 +5,11 @@ describe Dream do
   it "has a valid factory" do
     expect(FactoryGirl.create(:dream)).to be_valid
   end
-  it "is invalid without name"
-  it "is invalid without description"
+  it "is invalid without name" do
+    expect(FactoryGirl.build(:dream, name: nil)).not_to be_valid
+  end
+  it "is invalid without description" do
+    expect(FactoryGirl.build(:dream, description: nil)).not_to be_valid
+  end
   it "returns a dream's keywords as a string"
 end
