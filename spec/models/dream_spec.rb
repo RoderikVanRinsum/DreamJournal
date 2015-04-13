055945cceec898ab98c960d68fc40a5c0e0b9d87
Roderik van Rinsum <rhvanrinsum@gmail.com>
2015-04-13 10:37:33 +0200

    Add more dream validation tests with shoulda

--- spec/models/dream_spec.rb
+++ spec/models/dream_spec.rb
@@ -2,35 +2,57 @@
 require 'rails_helper'
 
 describe Dream do
-  
+  let!(:created_valid_dream) {FactoryGirl.create(:dream, name: "created_name", description: "desc")}
 
   it "has a valid factory" do
     expect(FactoryGirl.create(:dream)).to be_valid
   end
 
-  #validation tests
-  it "is invalid without name" do
-    expect(FactoryGirl.build(:dream, name: nil)).not_to be_valid
-  end
+  describe 'validations and relationships:' do
+    it { should have_many(:dreamsigns)}
+    it { should have_many(:dream_dreamsigns)}
 
-  it "is invalid without description" do
-    expect(FactoryGirl.build(:dream, description: nil)).not_to be_valid
-  end
+    context 'when name and description are present' do
+      subject {FactoryGirl.build(:dream, name: "built_name", description: "desc")}
 
-  it "has a unique name" do
-    FactoryGirl.create(:dream, name: "Double")
-    expect(FactoryGirl.build(:dream, name: "Double")).not_to be_valid
+      its(:name) { is_expected.not_to be_nil }
+      its(:description) { is_expected.not_to be_nil}
+      it { is_expected.to be_valid }
+    end
+
+    it { should validate_presence_of(:name) }
+
+    it "is invalid without name" do
+      expect(FactoryGirl.build(:dream, name: nil)).not_to be_valid
+    end
+
+    it { should validate_presence_of(:description)}
+
+    it "is invalid without description" do
+      expect(FactoryGirl.build(:dream, description: nil)).not_to be_valid
+    end
+
+    it { should validate_uniqueness_of(:name)}
+
+    it "has a unique name" do
+      expect(FactoryGirl.build(:dream, name: "created_name")).not_to be_valid
+    end
   end
 
   #instance methods
-  it "saves keywords after successful creation" do
-    dream = FactoryGirl.create(:dream, name: "Name", description: "desc")
-    expect(dream.keywords).to eq("name desc")
-  end
+  describe '#set_keywords' do
+    it "saves keywords after successful creation" do
+      expect(created_valid_dream.keywords).to eq("created_name desc")
+    end
 
-  it "saves keywords after successful update" do 
-    dream = FactoryGirl.create(:dream, name: "old", description: "desc")
-    expect{dream.update(name: "new")}.to change{dream.keywords}.from("old desc").to("new desc")
+    it "saves keywords after successful update" do 
+      expect {created_valid_dream.update(name: "new")}.to change {created_valid_dream.keywords}.from("created_name desc").to("new desc")
+    end
+
+    it "doesn't save keywords after update fails" do
+      allow_any_instance_of(Dream).to receive(:save).and_return(false)
+      expect {created_valid_dream.update(name: "new")}.not_to change {created_valid_dream.keywords}
+    end
   end
 
   #class methods & scopes
@@ -59,7 +81,7 @@ describe Dream do
 
     context 'when keyword is empty' do
       it "returns all results" do
-        expect(Dream.search("")).to eq [dream1, dream2, dream3]
+        expect(Dream.search("")).to eq [created_valid_dream, dream1, dream2, dream3]
       end
     end
     
